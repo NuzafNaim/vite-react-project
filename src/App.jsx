adf26d7b71219863d1bd2f342f6d27a279743e53
Nuzaf Naim Saikat <hello@nuzafnaim.dev>
2023-08-09 20:15:58 +0600

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Fragment } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+
+// Using only one condition && condition 
+const App = () => {
+  let isLogedIn = true;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Fragment>
+      {
+        isLogedIn && <button className='btn btn-success'>Dashboard</button>
+     }
+    </Fragment>
+  );
+};
 
-export default App
+
+
+
+
+// // Using map loop 
+// const App = () => {
+//   let items = ['A', 'B', 'C', 'D', 'E'];
+//   return (
+//     <Fragment>
+//       <ol>
+//       {
+//         items.map((eachItem) => {
+//           return <li key="">{eachItem}</li>
+//         })
+//         }
+//       </ol>
+//     </Fragment>
+//   );
+// };
+
+
+// If else condtion
+// const App = () => {
+//   let marks = 80;
+//   return (
+//     <Fragment>
+//       {
+//         (() => {
+//           if (marks >= 80) {
+//            return <h1>Passed</h1>
+//           } else {
+//             return <h1>Failed</h1>
+//          }
+//        })()
+//       }
+//     </Fragment>
+//   );
+// };
+
+export default App;
